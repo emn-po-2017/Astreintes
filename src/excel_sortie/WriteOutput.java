4d7f71fa79ebf35b56aff5d014f5150c958d2e5e
ASUS-X501A\Lucas <lucas.desvages@live.fr>
2017-02-13 11:39:02 +0100

    Message si Planning.xls existe déjà.

--- src/excel_sortie/WriteOutput.java
+++ src/excel_sortie/WriteOutput.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Locale;
 
+import javax.swing.JOptionPane;
+
 import excel_entrée.Read_Informations;
 import jxl.Workbook;
 import jxl.WorkbookSettings;
@@ -27,25 +29,51 @@ public class WriteOutput {
     public void write() throws IOException, WriteException {
     	//Création du document final
         File file = new File(this.path);
-        WorkbookSettings wbSettings = new WorkbookSettings();
-        wbSettings.setLocale(new Locale("en", "EN"));
-        WritableWorkbook workbook = Workbook.createWorkbook(file, wbSettings);
-            
-        //Ajout et écriture d'une feuille "Calendrier"
-        workbook.createSheet("Calendrier", 0);
-        WritableSheet _calendrier = workbook.getSheet(0);
-        Calendrier calendrier = new Calendrier(_calendrier, resultat_solver, infos);
-        calendrier.createCalendar();
-        
-        //Ajout et écriture d'une feuille pour chaque médecin
-        for (int i=0; i<infos.getDoctors().size(); i++) {
-        	workbook.createSheet(infos.getDoctors().get(i), i+1);
-            WritableSheet _calendrier_medecin = workbook.getSheet(i+1);
-            Calendrier_Medecin calendrier_medecin = new Calendrier_Medecin(_calendrier_medecin, resultat_solver, infos, i);
-            calendrier_medecin.createCalendar();
+        int retour = 1;
+        if(file.exists()){
+        	JOptionPane d = new JOptionPane();
+			retour = JOptionPane.showConfirmDialog(
+					d, 
+					"Le fichier Planning.xls existe déjà.\n"
+					+" \n"
+					+ "Voulez-vous l'écraser ?\n"
+					+" \n", 
+					"Attention",
+					JOptionPane.OK_CANCEL_OPTION);
+			if(retour == 0){
+				JOptionPane.showMessageDialog(
+					d, 
+					"Avant de continuer, vérifiez que vous avez fermé Planning.xls.\n"
+					+"\n", 
+					"Information",
+					JOptionPane.INFORMATION_MESSAGE);
+			}
         }
-
-        workbook.write();
-        workbook.close();
+        
+		if(retour == 0 || !file.exists() ){
+	        WorkbookSettings wbSettings = new WorkbookSettings();
+	        wbSettings.setLocale(new Locale("en", "EN"));
+	        WritableWorkbook workbook = Workbook.createWorkbook(file, wbSettings);
+	            
+	        //Ajout et écriture d'une feuille "Calendrier"
+	        workbook.createSheet("Calendrier", 0);
+	        WritableSheet _calendrier = workbook.getSheet(0);
+	        Calendrier calendrier = new Calendrier(_calendrier, resultat_solver, infos);
+	        calendrier.createCalendar();
+	        
+	        //Ajout et écriture d'une feuille pour chaque médecin
+	        for (int i=0; i<infos.getDoctors().size(); i++) {
+	        	workbook.createSheet(infos.getDoctors().get(i), i+1);
+	            WritableSheet _calendrier_medecin = workbook.getSheet(i+1);
+	            Calendrier_Medecin calendrier_medecin = new Calendrier_Medecin(_calendrier_medecin, resultat_solver, infos, i);
+	            calendrier_medecin.createCalendar();
+	        }
+	
+	        workbook.write();
+	        workbook.close();
+        }
+		else{ 
+			System.exit(0);
+		}
     }
 }
